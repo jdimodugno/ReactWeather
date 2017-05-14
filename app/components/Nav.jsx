94bffc0dc6c3d31d8239318666c39e98688a6506
Juan Di Modugno <dimodugnojuan@gmail.com>
2017-05-14 15:54:07 -0300

    Wired up the nav

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,8 +4,12 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function (e) {
     e.preventDefault();
-
-    alert('Not yet wired up!');
+    var location = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+    if (location.length > 0) {
+      this.refs.search.value = '';
+      window.location.hash = '#/?location=' + encodedLocation;
+    }
   },
   render: function () {
     return (
@@ -25,14 +29,14 @@ var Nav = React.createClass({
           </ul>
         </div>
         <div className='top-bar-right'>
-          <ul className="menu">
+          <ul className='menu'>
             <form onSubmit={this.onSearch}>
               <ul className='menu'>
                 <li>
-                  <input type="search" placeholder="Search weather by city"/>
+                  <input type='search' placeholder='Search weather by city' ref='search'/>
                 </li>
                 <li>
-                  <input type="submit" className="button" value='Get Weather'/>
+                  <input type='submit' className='button' value='Get Weather'/>
                 </li>
               </ul>
             </form>
